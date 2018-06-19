51d5c954b1598072e991d4bf502086230e2069aa
montapas <montapas@gmail.com>
2018-06-19 15:09:53 +0530

    eerre
    errere

--- src/main/java/com/capitalone/dashboard/collector/SonarClient.java
+++ src/main/java/com/capitalone/dashboard/collector/SonarClient.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface SonarClient {
 
     List<SonarProject> getProjects(String instanceUrl);
-//tapas
+//tapas mondal
     CodeQuality currentCodeQuality(SonarProject project);
 
 }
