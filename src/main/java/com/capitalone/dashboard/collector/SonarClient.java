736400eb5481d803b5a48179ff8d02b4c5c96392
montapas <montapas@gmail.com>
2018-06-19 15:23:37 +0530

    Update SonarClient.java

--- src/main/java/com/capitalone/dashboard/collector/SonarClient.java
+++ src/main/java/com/capitalone/dashboard/collector/SonarClient.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface SonarClient {
 
     List<SonarProject> getProjects(String instanceUrl);
-//tapas mondal
+//tapas mondal0
     CodeQuality currentCodeQuality(SonarProject project);
 
 }
